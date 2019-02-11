97d64baaf1d11bf8d7f76989e5025540c349a688
Ihsan Berahim <ihsanberahim@gmail.com>
2019-02-11 13:08:34 +0800

    improved the pligin ZIP structure

--- build.sh
+++ build.sh
@@ -1,8 +1,28 @@
-#/bin/bash
+#!/usr/bin/env bash
 
 composer dump
 
-rm -rf ib-laravel-helper.zip
-zip -r ib-laravel-helper.zip ./ -x "*.git*" -x  "*.idea*"
+PLUGIN_SLUG=ib-laravel-helper
 
-wp plugin install ib-laravel-helper.zip --activate --force --path=$WP_HOME
+# clean build
+mkdir $PLUGIN_SLUG
+rm -rf ib-laravel-helper
+touch ib-laravel-helper.zip
+rm -rf ib-laravel-helper.zip
+
+# rebuild
+zip -r ib-laravel-helper.zip ./ -x "*.git*" -x  "*.idea*"
+mkdir ib-laravel-helper
+mv ib-laravel-helper.zip ib-laravel-helper/
+cd ib-laravel-helper
+unzip ib-laravel-helper.zip
+rm ib-laravel-helper.zip
+cd ..
+zip -r ib-laravel-helper.zip ./ib-laravel-helper
+
+# clean build
+rm -rf ib-laravel-helper
+
+#wp plugin install ib-laravel-helper.zip --activate --force --path=$WP_HOME
+#wp plugin install ib-laravel-helper.zip --activate --force --path=/Users/ihsanberahim/Documents/MARYJARDIN/Sites/www.maryjardin
+wp plugin install ib-laravel-helper.zip --activate --force --path=/Users/ihsanberahim/Documents/Sites/mymayamart
--- ib-laravel-helper.php
+++ ib-laravel-helper.php
@@ -7,7 +7,7 @@
  * Author URI:      https://internetbacked.com
  * Text Domain:     ib-laravel-helper
  * Domain Path:     /languages
- * Version:         1.2.0
+ * Version:         1.2.1
  *
  * @package         Ib_Laravel_Helper
  */
